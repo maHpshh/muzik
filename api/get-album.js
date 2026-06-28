4e9433c7312f806514ff5a81f8f4e7b6e24f7ecd
maHpshh <litaehun616@gmail.com>
2026-06-29 03:19:29 +0800

    Update get-album.js

--- api/get-album.js
+++ api/get-album.js
@@ -1,12 +1,8 @@
 module.exports = async (req, res) => {
 
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "POST only" });
-  }
-
-  const { albumId } = req.body;
-
   try {
+
+    const { albumId } = req.body;
 
     const r = await fetch(
       `https://itunes.apple.com/lookup?id=${albumId}&entity=song`
@@ -17,6 +13,6 @@ module.exports = async (req, res) => {
     return res.status(200).json(data);
 
   } catch (e) {
-    return res.status(500).json({ error: e.message });
+    return res.status(500).json({});
   }
 };
